7ce2661d3a32917d4b73f37865287d5039902eed
sac086 <sachincsrb@gmail.com>
2024-03-19 11:50:35 +0530

    confilct

--- src/test/java/copyedfiles/datadriven.java
+++ src/test/java/copyedfiles/datadriven.java
@@ -1,24 +1,35 @@
 package copyedfiles;
 
 public class datadriven {
+	int a=10;
+	int b=11;
+	int c=12;
+	int d=13;
+	static int e=14;
+	
 	public static void main(String[] args) {
-		System.out.println("dddfgfshddf");
+		try {
+			System.out.println("dddfgfshddf"+ e);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 System.out.println("ggjhhjkj");
 System.out.println("dsdaf");
 }
  
 	public void gm(){
-		System.out.println("hi");
+		System.out.println("hi"+a);
 	}
 
 	public void gn(){
-		System.out.println("bye");
+		System.out.println("bye"+b);
 	}
 	public void vgn(){
-		System.out.println("bye....");
+		System.out.println("bye...."+c);
 	}
 	public void vgm(){
-		System.out.println("hi...");
+		System.out.println("hi..."+d);
 	}
 
 }
