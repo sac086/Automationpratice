85d0bcd7f6d907e7605dad1ccdb65ee53aafcfbd
sac086 <sachincsrb@gmail.com>
2024-03-19 11:44:29 +0530

    conflict

--- src/test/java/copyedfiles/datadriven.java
+++ src/test/java/copyedfiles/datadriven.java
@@ -4,4 +4,21 @@ public class datadriven {
 	public static void main(String[] args) {
 		System.out.println("dddfgfshddf");
 System.out.println("ggjhhjkj");
-}}
+System.out.println("dsdaf");
+}
+ 
+	public void gm(){
+		System.out.println("hi");
+	}
+
+	public void gn(){
+		System.out.println("bye");
+	}
+	public void vgn(){
+		System.out.println("bye....");
+	}
+	public void vgm(){
+		System.out.println("hi...");
+	}
+
+}
